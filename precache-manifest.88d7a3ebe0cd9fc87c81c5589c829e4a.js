25c8b0bb478279ab6f747e4c182ecbf15893dd15
sosisvesalam <colakilgaz@gmail.com>
2018-12-31 01:02:48 +0300

    Updates

---
+++ precache-manifest.88d7a3ebe0cd9fc87c81c5589c829e4a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7632a33a7f36e8fd18e",
+    "url": "/deneme123/static/js/main.d7632a33.chunk.js"
+  },
+  {
+    "revision": "ebfeabc70db4f14c7982",
+    "url": "/deneme123/static/js/1.ebfeabc7.chunk.js"
+  },
+  {
+    "revision": "b1fbc152f47bd5f16d25",
+    "url": "/deneme123/static/js/runtime~main.b1fbc152.js"
+  },
+  {
+    "revision": "8dab1c238d5f684c7ca8c9031b66a79c",
+    "url": "/deneme123/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deneme123/precache-manifest.094a6482eecd83cf89ce45baedb27f34.js"
+  "/deneme123/precache-manifest.88d7a3ebe0cd9fc87c81c5589c829e4a.js"
 );
 
 workbox.clientsClaim();
