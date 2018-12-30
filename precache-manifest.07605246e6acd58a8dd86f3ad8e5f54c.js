4d8671056343fe0db24f5d55e8bed829da952d32
sosisvesalam <colakilgaz@gmail.com>
2018-12-30 23:20:13 +0300

    Updates

---
+++ precache-manifest.07605246e6acd58a8dd86f3ad8e5f54c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bfc372d390d425fa994",
+    "url": "/deneme123/static/js/main.8bfc372d.chunk.js"
+  },
+  {
+    "revision": "2ac0e71e52fabae6c4be",
+    "url": "/deneme123/static/js/1.2ac0e71e.chunk.js"
+  },
+  {
+    "revision": "b1fbc152f47bd5f16d25",
+    "url": "/deneme123/static/js/runtime~main.b1fbc152.js"
+  },
+  {
+    "revision": "1fdc693a7e70777651bbbbc8b91d8477",
+    "url": "/deneme123/index.html"
+  }
+];
